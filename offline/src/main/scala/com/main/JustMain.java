f5aeeb53a6aad219bfa4fcee68c91d0bc26d3511
y1 <cq.yh@qq.com>
2022-01-27 12:02:57 +0800

    add main func

---
+++ offline/src/main/scala/com/main/JustMain.java
@@ -0,0 +1,7 @@
+package com.main;
+
+public class JustMain {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ realtime/src/main/scala/com/training/dw/main/JustMain.scala
@@ -0,0 +1,8 @@
+package com.training.dw.main
+
+
+object JustMain {
+  def main(args: Array[String]): Unit = {
+    println("Hello Scala")
+  }
+}
