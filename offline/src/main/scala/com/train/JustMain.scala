a4757761b1f479058fa00014ca0f74e8db0d1676
y1 <cq.yh@qq.com>
2022-04-23 23:22:48 +0800

    add code

--- offline/src/main/scala/com/train/JustMain.scala
+++ offline/src/main/scala/com/train/JustMain.scala
@@ -19,7 +19,12 @@ object JustMain {
   }
 
   def main(args: Array[String]): Unit = {
+    if (args.length != 2) {
+      println("请输入参数！参数一：需要操作的数仓层级；参数二：操作的分区日期")
+      System.exit(1)
+    }
     run1
     run2
+    run3
   }
 }
